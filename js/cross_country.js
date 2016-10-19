cd4c43d6abaaefa3ce0b4d8a90d8c43aa61961e2
Brandon Buchanan <Brandon Buchanan>
2016-10-19 08:39:33 -0600

    Major changes prior to launch

--- js/cross_country.js
+++ js/cross_country.js
@@ -1,13 +1,14 @@
 // JavaScript Document
 var isViewing = false;
 function expandBox(boxId) {
-	
+
+    var dataname = $(boxId).attr("data-name");
+
+    runCounter(dataname);
+	console.log(dataname);
   	var boxImg = $(boxId).next();
   	var parentId = $(boxId).parent();
 	var boxImgFull = $(boxId).siblings(".dwn-full-img");
-    console.log(boxImgFull);
-	
-	console.log(parentId);
 	
   	// $(boxImg).css('height', '500px');
   	// $(boxImg).css('width', '500px');
@@ -19,8 +20,7 @@ function expandBox(boxId) {
 	$(boxImg).css('border-radius', '10px');
 	
 	var boxTitle = $(parentId).find('.dwn-main-title');
-	console.log(boxTitle);
-	
+
 	
 	$(boxTitle).css('z-index', '20002');
 	$(boxTitle).css('top', '70px');
@@ -96,7 +96,7 @@ $(document).ready(function() {
     $('#save-btn').click(function() {
 	  $('#saver').hide();
 	  $('#menu').show();
-		isViewing = true;
+		checkInactivity();
   });
   
   
@@ -140,3 +140,68 @@ $(document).ready(function() {
 
 	
 });
+
+
+/*
+COOKIES
+ */
+
+//
+// // create an instance of the Date object
+// var now = new Date();
+// // fix the bug in Navigator 2.0, Macintosh
+//
+// /*
+//  cookie expires in one year (actually, 365 days)
+//  365 days in a year
+//  24 hours in a day
+//  60 minutes in an hour
+//  60 seconds in a minute
+//  1000 milliseconds in a second
+//  */
+//
+// function runCounter(countName)
+// {
+//     now.setTime(now.getTime() + 365 * 24 * 60 * 60 * 1000);
+//     var visits = getCookie(countName);
+//
+//     // if the cookie wasn't found, this is your first visit
+//     if (!visits) {
+//         visits = 1; // the value for the new cookie
+//         console.log('first time making cookie');
+//     } else {
+//         // increment the counter
+//         visits = parseInt(visits) + 1;
+//         console.log("Made cookies " + visits + " times");
+//     }
+//     // set the new cookie
+//     setCookie(countName, visits, now);
+// }
+//
+// function getCookie(name) {
+//     var dc = document.cookie;
+//     var prefix = name + "=";
+//     var begin = dc.indexOf("; " + prefix);
+//     if (begin == -1) {
+//         begin = dc.indexOf(prefix);
+//         if (begin != 0) return null;
+//     } else
+//         begin += 2;
+//     var end = document.cookie.indexOf(";", begin);
+//     if (end == -1)
+//         end = dc.length;
+//     return unescape(dc.substring(begin + prefix.length, end));
+// }
+//
+// function setCookie(name, value, expires, path, domain, secure) {
+//     var curCookie = name + "=" + escape(value) +
+//         ((expires) ? "; expires=" + expires.toGMTString() : "") +
+//         ((path) ? "; path=" + path : "") +
+//         ((domain) ? "; domain=" + domain : "") +
+//         ((secure) ? "; secure" : "");
+//     document.cookie = curCookie;
+//     console.log(curCookie);
+//     console.log(document.cookie);
+// }
+
+// -->
--- js/inactivity.js
+++ js/inactivity.js
@@ -1,25 +1,46 @@
+
+/*
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                        CONFIG
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ */
+var refreshMinutes = 1;
+
+//starts the idleTime count at 0
 var idleTime = 0;
 
-$(document).ready(function () {
-    if (isViewing)
-    {
-        //Increment the idle time counter every minute.
-        var idleInterval = setInterval(timerIncrement, 5000); // 1 minute
 
-        //Zero the idle timer on mouse movement.
-        $(this).mousemove(function (e) {
-            idleTime = 0;
-        });
-        $(this).keypress(function (e) {
-            idleTime = 0;
-        });
-    }
+//This function constant checks the application for mouse movement
+//or key pressing to reset the idle time.  This will call the timerIncrement
+//function when 60 seconds is up.
+function checkInactivity()
+{
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
 
-});
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
 
+    //Zero the idle timer everytime a key is pressed
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+}
+
+
+//This function will see if the current idle timer is greater than
+//the number of minutes defined in the config section.
 function timerIncrement() {
+
+    //Increment the timer every time it's called
     idleTime = idleTime + 1;
-    if (idleTime > 1) { // 1 minutes
+
+    //if the timer is greater than the minutes defined then reload the page.
+    if (idleTime > refreshMinutes) { // 1 minutes
         window.location.reload();
     }
 }
